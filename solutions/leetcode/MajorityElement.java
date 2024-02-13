b084611a9cfb2c5d6b75c9dce012af35202d3035
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-02-13 12:08:39 +0900

    [ADD] leetcode solutions

---
+++ solutions/leetcode/MajorityElement.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int max = 0;
+        int maxCount = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int num : nums) {
+            if (m.get(num) != null) {
+                m.put(num, m.get(num) + 1);
+                if (m.get(num) > maxCount) {
+                    max = num;
+                    maxCount=m.get(max);
+                }
+            } else {
+                m.put(num, 1);
+                if (m.get(num) > maxCount) {
+                    maxCount = m.get(num);
+                }
+            }
+        }
+        return max;
+    }
+}
