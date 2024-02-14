4c48827af5dfd32631124fe647cca2e7aedc2aa9
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-02-14 14:32:25 +0900

    *ADD* LeetCode_80

---
+++ solutions/leetcode/LeetCode_80.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+// 80. Remove Duplicates from Sorted Array II
+
+public class LeetCode_80 {
+    public int removeDuplicates(int[] nums) {
+        int k = 1, a = 0, b = 1;
+        while (b < nums.length) {
+            int count = 0;
+            if (nums[b] != nums[a] && nums[a + 1] != nums[a]) {
+                a++;
+                nums[a] = nums[b];
+                k++;
+            } else if(nums[b] != nums[a] && nums[a + 1] == nums[a]){
+                nums[a + 1] = nums[a];
+                nums[a + 2] = nums[b];
+                a += 2;
+                k++;
+            }
+            b++;
+        }
+        return k;
+    }
+}
