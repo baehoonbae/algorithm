12b06ebe88acddbc769b7b089357a804181cb8dc
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-02-20 10:40:36 +0900

    *ADD* LeetCode #268

---
+++ solutions/leetcode/LeetCode_268.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+// 268. Missing Number
+
+// Runtime: 0 ms
+// Memory: 44.74 MB
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+public class LeetCode_268 {
+    public int missingNumber(int[] nums) {
+        boolean[] arr = new boolean[nums.length + 1];
+        for (int num : nums) {
+            arr[num] = true;
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            if (!arr[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
