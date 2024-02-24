48d4fc04a49b281ca84adff8b1a6ccf314f659a7
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-02-24 14:39:04 +0900

    *ADD* LeetCode #45

---
+++ solutions/leetcode/LeetCode_45.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Arrays;
+
+// 45. Jump Game II
+
+// Runtime: 2 ms
+// Memory: 44.63 MB
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+public class LeetCode_45 {
+    public int canJump(int[] nums) {
+        int jump = 0;
+        int maxReach = 0;
+        int prevMaxReach = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return jump + 1;
+            }
+            if (i == prevMaxReach) {
+                prevMaxReach = maxReach;
+                jump++;
+            }
+        }
+        return jump;
+    }
+}
