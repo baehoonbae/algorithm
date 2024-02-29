ff6b8356c55c27945d6515a2b76b55c899cfc783
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-02-29 16:29:52 +0900

    *ADD* 10진법 -> N진법으로 바꾸기

---
+++ library/numeral/TenToN.java
@@ -0,0 +1,19 @@
+package libraries.numeral;
+
+public class TenToN {
+    public static StringBuilder conversionToN(int number, int n) {
+        StringBuilder sb = new StringBuilder();
+        int current = number;
+        while (current > 0) {
+            // current % n이 10보다 큰 경우, 대문자 알파벳으로 나오기 때문에 적절히 변환해야함.
+            // 0~9 까지는 숫자 그대로 나온다.
+            if (current % n < 10) {
+                sb.append(current % n);
+            } else {
+                sb.append((char) (current % n - 10 + 'A'));
+            }
+            current /= n;
+        }
+        return sb.reverse();
+    }
+}
