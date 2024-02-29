6b98cc4c0a833c8179be24db94b19e537197850d
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-02-29 16:47:55 +0900

    *UPDATE* N진법 -> 10진법 변환

--- library/numeral/NToTen.java
+++ library/numeral/NToTen.java
@@ -3,13 +3,9 @@ package libraries.numeral;
 public class NToTen {
     public static int conversionNToTen(StringBuilder num, int n) {
         int result = 0;
-        char[] arr = new char[num.length()];
         StringBuilder sb = num.reverse();
         for (int i = 0; i < sb.length(); i++) {
-            arr[i] = sb.charAt(i);
-        }
-        for (char c : arr) {
-            result += (int) Math.pow(n, c);
+            result += (int) (Math.pow(n, i)) * (sb.charAt(i) - '0');
         }
         return result;
     }
