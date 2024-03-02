bb67b4f00955c3da92f77a711c1bdd4d98e3cf3e
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-03-02 13:16:11 +0900

    *ADD* LeetCode #977
    977. Squares of a Sorted Array

---
+++ solutions/leetcode/LeetCode_977.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+// 977. Squares of a Sorted Array
+
+// Runtime: 7 ms
+// Memory: 46.20 MB
+// TC: O(nlogn)
+// SC: O(n)
+
+import java.util.Arrays;
+
+public class LeetCode_977 {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
