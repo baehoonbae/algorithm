979051f9a65371424c6231d69f25033132d1885f
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-04-29 17:10:23 +0900

    feat: 릿코드 #2997

--- src/leetcode/LeetCode_2997.java
+++ src/leetcode/LeetCode_2997.java
@@ -17,7 +17,6 @@ package leetcode;
 
 public class LeetCode_2997 {
     public int minOperations(int[] nums, int k) {
-        int count = 0;
         int n = 0;
         for (int i = 0; i < nums.length; i++) {
             n ^= nums[i];
