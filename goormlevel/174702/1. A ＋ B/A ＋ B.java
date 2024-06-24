d7a62cd4673fd1d4950d851d6b8aaaa15938906c
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-06-24 19:14:45 +0900

    [goormlevel] Title: A + B, Time: 0.15 ms, Memory: 34.92 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/174702/1. A ＋ B/A ＋ B.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		sc.nextLine();
+		System.out.println(a+b);
+	}
+}
